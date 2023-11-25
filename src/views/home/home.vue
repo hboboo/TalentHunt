99679989599f582deba210133bfbf7c06a841b86
hb <515870153@qq.com>
2023-11-25 17:47:35 +0800

    完成-首页筛选、样式

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -7,19 +7,19 @@
             <span :class="{ active_job_type: changeShow == 'career' }" @click="handleClick('career', true)">全职</span>
           </div>
           <div>
-            <span :class="{ active_job_type: changeShow == 'internship' }" @click="handleClick('internship', false)"
-              >实习</span
-            >
+            <span :class="{ active_job_type: changeShow == 'internship' }" @click="handleClick('internship', false)">实习</span>
           </div>
-          <van-icon name="search" class="search" />
+          <div class="identity-container" @click="goHomePage">
+            <span>{{identityText}}<van-icon name="arrow" /></span>
+          </div>
+          <van-icon name="search" class="search" color="#060639" @click="goSearch"/>
         </div>
         <div class="recommendation-title">
           <span :class="{ active_content_type: content == 'complete' }" @click="selectContent('complete')">全部</span>
           <span :class="{ active_content_type: content == 'popular' }" @click="selectContent('popular')">热门</span>
-          <div class="button-container">
-            <div>
-              <button>{{ siev }}<van-icon name="arrow-down" @click="showSiev" /></button>
-            </div>
+          <div class="button-container" @click="showSiev" :class="{ 'active': isButtonActive }">
+            <span>筛选</span>
+            <van-icon name="arrow-down"/>
           </div>
         </div>
       </section>
@@ -41,14 +41,60 @@
       </section>
       <transition name="slide">
         <div class="siev-container" v-if="issiev">
-          <van-popover v-model="showPopover" trigger="click" :actions="actions" @select="onSelect">
-            <template #reference>
-              <van-button type="primary">浅色风格</van-button>
-            </template>
-          </van-popover>
+          <van-field
+            readonly
+            clickable
+            name="学历要求"
+            :value="job_education"
+            label="学历要求"
+            placeholder="点击选择学历区间"
+            @click="showPickerEducation = true"
+          />
+          <van-popup v-model="showPickerEducation" position="bottom" round>
+            <van-picker
+              show-toolbar
+              :columns="educationArray"
+              @confirm="onConfirmEducation"
+              @cancel="showPickerEducation = false"
+            />
+          </van-popup>
+          <van-field
+            readonly
+            clickable
+            name="薪资"
+            :value="salary"
+            label="薪资"
+            placeholder="点击选择薪资区间"
+            @click="showPickerSalary = true"
+          />
+          <van-popup v-model="showPickerSalary" position="bottom" round>
+            <van-picker
+              show-toolbar
+              :columns="salaryArray"
+              @confirm="onConfirmSalary"
+              @cancel="showPickerSalary = false"
+            />
+          </van-popup>
+          <van-field
+            readonly
+            clickable
+            name="工作经验"
+            :value="job_experience"
+            label="工作经验"
+            placeholder="点击选择经验区间"
+            @click="showPickerExperience = true"
+          />
+          <van-popup v-model="showPickerExperience" position="bottom" round>
+            <van-picker
+              show-toolbar
+              :columns="experience"
+              @confirm="onConfirmExperience"
+              @cancel="showPickerExperience = false"
+            />
+          </van-popup>
           <div class="confirm-container">
-            <span class="eliminate">清除</span>
-            <span class="confirm">确定</span>
+            <span class="eliminate" @click="clearFilters">清除</span>
+            <span class="confirm" @click="confirmFilters">确定</span>
           </div>
         </div>
       </transition>
@@ -57,7 +103,7 @@
 </template>
 
 <script>
-import { Icon, Tabbar, TabbarItem, Swipe, SwipeItem, Popover ,Button} from "vant";
+import { Icon, Tabbar, TabbarItem, Swipe, SwipeItem, Field, Popup, Picker} from "vant";
 import companyList from "../../components/common/companyList.vue";
 export default {
   name: "Home",
@@ -66,21 +112,35 @@ export default {
     return {
       changeShow: "career", //默认选取全职
       content: "", //默认选取全部
-      siev: "筛选",
       images: ["https://img01.yzcdn.cn/vant/apple-1.jpg", "https://img01.yzcdn.cn/vant/apple-2.jpg"],
       jobList: [], //渲染列表
       jobType: "", //储存第一次筛选的结果
       issiev: false,
-      showPopover: false,
-      // 通过 actions 属性来定义菜单选项
-      actions: [{ text: '选项一' }, { text: '选项二' }, { text: '选项三' }],
+      showPickerEducation: false, //是否显示学历选择器
+      job_education: "", //学历
+      educationArray: ["初中以下", "中专", "高中", "大专", "本科", "硕士", "博士"],
+      job_experience: "", //工作经验
+      showPickerExperience: false, //是否显示经验选择器
+      experience: ["在校生", "应届生", "1年以内", "1-3年", "3-5年", "5-10年", "10年以上"],
+      salary: "", //薪资
+      showPickerSalary: false, //薪资选择器
+      salaryArray: ["3k以下", "3-5k", "5-10k", "10-20k", "20k以上"], //薪资选择器数据
+      isButtonActive: false,
+      role: ''
     };
   },
   created() {
     this.getRecommendation(true);
+    this.role = localStorage.getItem('role');
   },
 
   mounted() {},
+
+  computed: {
+    identityText() {
+      return this.role === 'jobSeeker' ? '应聘版' : '招聘版';
+    },
+  },
 
   methods: {
     //显示样式的切换&&传递一个查询参数请求招聘信息
@@ -125,11 +185,59 @@ export default {
 
     showSiev() {
       this.issiev = !this.issiev;
+      this.isButtonActive = !this.isButtonActive;
     },
 
-    onSelect(action) {
-      console.log(action.text);
+    confirmFilters() {
+      // 向后端发送带有选择的过滤器值的请求
+      // 根据你的后端 API，修改端点和参数
+      this.$http
+        .post("/job/filtrate", {
+          job_education: this.job_education,
+          job_experience: this.job_experience,
+          salary: this.salary,
+        })
+        .then((res) => {
+          // 处理后端返回的数据，可能需要更新 jobList 或者其他数据
+          this.jobList = res.data
+          this.issiev = false
+          this.$forceUpdate();
+        })
+        .catch((error) => {
+          // 处理错误
+          console.error("Error while fetching filtered data:", error);
+        });
     },
+
+    clearFilters() {
+      // 重置所有选择的值为初始空字符串状态
+      this.job_education = "";
+      this.job_experience = "";
+      this.salary = "";
+    },
+
+    //薪资选择器
+    onConfirmSalary(value) {
+      this.salary = value;
+      this.showPickerSalary = false;
+    },
+    //学历选择器
+    onConfirmEducation(value) {
+      this.job_education = value;
+      this.showPickerEducation = false;
+    },
+    //工作经验选择器
+    onConfirmExperience(value) {
+      this.job_experience = value;
+      this.showPickerExperience = false;
+    },
+
+    goHomePage() {
+      this.$router.push('/homePage')
+    },
+    goSearch() {
+      this.$router.push('/search')
+    }
   },
 
   components: {
@@ -139,8 +247,9 @@ export default {
     [Swipe.name]: Swipe,
     [SwipeItem.name]: SwipeItem,
     companyList,
-    [Popover.name]: Popover,
-    [Button.name]: Button,
+    [Field.name]: Field,
+    [Popup.name]: Popup,
+    [Picker.name]: Picker,
   },
 };
 </script>
@@ -158,6 +267,7 @@ export default {
   .header-nav {
     display: flex;
     flex-direction: row;
+    position: relative;
     align-items: center;
     padding-top: 0.15rem;
     .wh(100%, 1.95rem);
@@ -194,6 +304,7 @@ export default {
       display: flex;
       margin-left: 6rem;
       align-items: center;
+      padding-right: 0.3rem;
       .button-left {
         margin-right: 0.9rem;
       }
@@ -234,6 +345,21 @@ export default {
       color: #fff;
     }
   }
+}
+.identity-container {
+  position: absolute;
+  right: 2rem;
+  span {
+    .sc(0.5rem, #333);
+    font-weight: 550;
+    border: solid 1px #bfb8b8;
+    border-radius: 0.2rem;
+    padding: 0 0.2rem;
+  }
+}
+.active {
+  border: solid 1px #bfb8b8;
+  border-radius: 0.2rem;
 }
 .slide-enter-active,
 .slide-leave-active {
